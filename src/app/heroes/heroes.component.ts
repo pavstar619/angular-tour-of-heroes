8722585091ebd9aec84a80e29f5fbbf2c4d2043c
Pavel Rahman <pavelx16@gmail.com>
2023-09-24 00:29:08 +0600

    add separate get hero details page

--- src/app/heroes/heroes.component.ts
+++ src/app/heroes/heroes.component.ts
@@ -1,7 +1,6 @@
 import { Component } from '@angular/core';
 import { HeroService } from '../hero.service';
 import { Hero } from '../hero'
-import { MessageService } from 'app/message.service';
 
 @Component({
   selector: 'app-heroes',
@@ -9,23 +8,18 @@ import { MessageService } from 'app/message.service';
   styleUrls: ['./heroes.component.css']
 })
 export class HeroesComponent {
-
-  selectedHero?: Hero;
   heroes: Hero[] = [];
 
-  constructor(private heroService: HeroService, private messageService: MessageService) { }
+  constructor(
+    private heroService: HeroService,
+  ) { }
 
   ngOnInit(): void {
     this.getHeroes();
   }
 
-  onSelect(hero: Hero) {
-    this.selectedHero = hero;
-    this.messageService.add(`HeroesComponent: Selected hero id=${hero.id}`);
-  }
-
   getHeroes(): void {
     this.heroService.getHeroes()
-        .subscribe(heroes => this.heroes = heroes);
+      .subscribe(heroes => this.heroes = heroes);
   }
 }
